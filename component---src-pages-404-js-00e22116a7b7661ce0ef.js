f3e3bbc9c1e5bf6c3b45a5261f654b385adedfd4
sparsh229 <sparsh.gupta229@gmail.com>
2021-07-09 22:44:58 +0530

    Deployment

---
+++ component---src-pages-404-js-00e22116a7b7661ce0ef.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{w2l6:function(e,a,n){"use strict";n.r(a);var t=n("q1tI"),r=n.n(t),c=n("7oih"),l=n("Wbzz");n("EYWl");a.default=function(){return r.a.createElement(c.a,null,r.a.createElement("main",{className:"error-page"},r.a.createElement("div",{className:"error-container"},r.a.createElement("h1",null,"oops it's a dead end"),r.a.createElement(l.a,{to:"/",className:"btn"},"back home"))))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-00e22116a7b7661ce0ef.js.map
